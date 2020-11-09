ecb270861505c949c23949f7a9c1a15b56c42d8f
Kuntiarso <gilang.grk17@gmail.com>
2020-11-09 16:08:00 +0700

    first commit | postgres

---
+++ postgres/comparison.sql
@@ -0,0 +1,9 @@
+-- Comparison in posgres, can be used after WHERE command
+
+SELECT 1 = 1; --return true
+
+SELECT 1 <> 1; --return false, that symbol means not equal
+
+SELECT 'kuntiarso' = 'kuntiarso'; --return true
+
+SELECT 'kuntiarso' = 'kuntia'; --return false
